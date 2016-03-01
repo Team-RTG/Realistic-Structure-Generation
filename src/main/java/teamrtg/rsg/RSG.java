3c05ae207e2246894160107d0b94093fdb489d64
Tobias Pisani <tobiaspisani@gmail.com>
2016-03-01 01:11:42 +0100

    Installed forge

---
+++ src/main/java/teamrtg/rsg/RSG.java
@@ -0,0 +1,20 @@
+package teamrtg.rsg;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = RSG.MODID, version = RSG.VERSION)
+public class RSG
+{
+    public static final String MODID = "rsg";
+    public static final String VERSION = "0.0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
